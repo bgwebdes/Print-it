b2f69d2dbc982c00a56af3b33244053959fe41e2
bgwebdes <118448947+bgwebdes@users.noreply.github.com>
2023-03-10 17:29:02 +0100

    ajout des commentaires

--- assets/script.js
+++ assets/script.js
@@ -21,56 +21,21 @@ const slides = [
 let numeroImage = 0;
 
 
-/* DECLARATION DES CONSTANTES UTILISEES DANS LE DOM */
 
 
-//déclaration de la constante des flèches dans le DOM
+//déclaration des constantes "next" et "previous" des flèches dans le DOM
 const next = document.querySelector('#arrow_right');
 const previous = document.querySelector('#arrow_left');
 
 
 
+/* CREATION DES FONCTIONS SUR LES FLECHES */
 
 
-/* AJOUT EVENEMENT AU CLIC SUR LES FLECHES */
-
-//ajout évènement au clic sur flèche suivante (droite)
-next.addEventListener('click', function () {
-
-
-	//fonctions mise à jour : image, texte, bullet point
-	nextSlide();//fonction principale qui détermine le numéro de changement d'image
-	imageUpdate();
-	textUpdate();
-	updateDots();
-	//fonction slide suivante
-	//console.log pour tester au clic
-	console.log('next arrow OK');
-
-
-});
-
-//ajout évènement au clic sur flèche précédente (gauche)
-
-previous.addEventListener('click', function () {
-
-	//console.log pour tester au clic
-	console.log('previous arrow OK');
-	prevSlide();//fonction principale qui détermine le numéro de changement d'image
-	imageUpdate();
-	textUpdate();
-	updateDots();
-	//fonction slide précédente
-	console.log('fonction prevSlide');
-
-
-});
-
-
-//fonction nextSlide : affiche slide suivante
+//fonction nextSlide : affiche slide suivante au clic sur la flèche de droite
 
 function nextSlide() {
-	// Si la position de l'image courante est inférieure à la longueur du tableau - 1 (soit 4-1 = position 3)
+	// Si la position de l'image courante est inférieure à la longueur du tableau - 1 (>>> 4-1 = position 3)
 	if (numeroImage < slides.length - 1) {
 		// Alors l'image suivante sera affichée
 		numeroImage = numeroImage + 1;
@@ -78,21 +43,21 @@ function nextSlide() {
 		// Sinon l'image courante = 0, on retourne à l'index 0 du tableau
 		numeroImage = 0;
 	}
-	
+
 	console.log('fonction nextSlide');
 
 
 }
-//fonction prevSlide : affiche slide précédente
+//fonction prevSlide : affiche slide précédente au clic sur la flèche de gauche
 function prevSlide() {
 
 	//si l'image courante est supérieure à l'index 0
 	if (numeroImage > 0) {
 
 		//on revient sur l'image précédente	
-		numeroImage = numeroImage - 1; 
-	} 	else {
-		//sinon on recule d'une image dans le tableau
+		numeroImage = numeroImage - 1;
+	} else {
+		//sinon on affiche la dernière image du tableau
 		numeroImage = slides.length - 1;
 	}
 	console.log('diapo courante =', numeroImage);
@@ -101,31 +66,32 @@ function prevSlide() {
 
 
 
+/* CREATION DES FONCTIONS DE MISE A JOUR DE LA BANNIERE */
 
 
-//fonction insertion de la nouvelle slide dans la bannière
+//fonction insertion et mise à jour de la nouvelle slide dans la bannière
 function imageUpdate() {
 
-	//déclaration de l'emplacement de l'image dans le DOM
+	//déclaration de l'emplacement de l'image dans le DOM sur la classe ".banner-img"
 	let imageInsertion = document.querySelector(".banner-img");
 
-	//constante pour déterminer l'image actuellement affichée
+	//déclaration d'une variable "currentImage" pour déterminer l'image actuellement affichée
 	let currentImage = slides[numeroImage];
 
-	//url de l'image + nom de l'image dans le tableau
+	//déclaration de l'url de l'image (source dans le dossier "assets") + nom de l'image dans le tableau
 	imageInsertion.src = "./assets/images/slideshow/" + currentImage.image;
 
-	//texte alternatif de l'image dans le tableau
+	//insertion du texte correspondant à l'image courante dans le tableau
 	imageInsertion.alt = currentImage.tagLine;
 
 }
 
-// fonction mise à jour des textes
+// fonction mise à jour des textes avec sélection de l'id " #banner-txt " dans le DOM
 
 function textUpdate() {
-	//emplacement pour les textes dans le DOM
+	//variable pour l'emplacement pour les textes dans le DOM
 	let text = document.querySelector("#banner-txt");
-	//texte actuellement affiché
+	// variable pour le texte actuellement affiché
 	let currentText = slides[numeroImage];
 	//insertion du texte correspondant au tableau et à l'image courante
 	text.innerHTML = currentText.tagLine;
@@ -158,6 +124,41 @@ function updateDots() {
 	}
 }
 
+/* AJOUT FONCTIONS EXECUTEES AU CLIC SUR LES FLECHES */
+
+//ajout des fonctions au clic sur flèche à droite
+next.addEventListener('click', function () {
+
+
+	//fonctions mise à jour : image, texte, bullet point
+	nextSlide();//fonction principale qui détermine le numéro de changement d'image (à exécuter en premier)
+	imageUpdate();
+	textUpdate();
+	updateDots();
+	//fonction slide suivante
+	//console.log pour tester au clic
+	console.log('next arrow OK');
+
+
+});
+
+//ajout des fonctions au clic sur flèche à gauche
+
+
+
+previous.addEventListener('click', function () {
+
+	//console.log pour tester au clic
+	console.log('previous arrow OK');
+	prevSlide();//fonction principale qui détermine le numéro de changement d'image (à exécuter en premier)
+	imageUpdate();
+	textUpdate();
+	updateDots();
+	//fonction slide précédente
+	console.log('fonction prevSlide');
+
+
+});
 
 
 
